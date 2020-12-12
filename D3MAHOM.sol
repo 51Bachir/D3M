6e9e1da88c001905fa65bdd5670fbc03134afdaf
Mohamed Sibachir <75868538+51Bachir@users.noreply.github.com>
2020-12-12 01:32:03 -0500

    Update D3MAHOM.sol

--- D3MAHOM.sol
+++ D3MAHOM.sol
@@ -1,11 +1,57 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.4.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "browser/ERC20.sol";
 
-contract D3MAHOM is ERC20, ERC20Detailed {
-
-    constructor () public ERC20Detailed("D3MAHOM", "D3M", 7) {
-        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+contract D3MAHOM is ERC20 {
+    string public constant symbol = "D3M";
+    string public constant name = "D3MAHOM";
+    uint8 public constant decimals = 7;
+    
+    uint private constant __totalSupply = 1000;
+    mapping (address => uint) private __balanceOf;
+    mapping (address => mapping (address => uint)) private __allowances;
+    
+    constructor() public {
+            __balanceOf[msg.sender] = __totalSupply;
+    }
+    
+    function totalSupply() public constant returns (uint _totalSupply) {
+        _totalSupply = __totalSupply;
+    }
+    
+    function balanceOf(address _addr) public constant returns (uint balance) {
+        return __balanceOf[_addr];
+    }
+    
+    function transfer(address _to, uint _value) public returns (bool success) {
+        if (_value > 0 && _value <= balanceOf(msg.sender)) {
+            __balanceOf[msg.sender] -= _value;
+            __balanceOf[_to] += _value;
+            return true;
+        }
+        return false;
+    }
+    
+    function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
+        if (__allowances[_from][msg.sender] > 0 &&
+            _value > 0 &&
+            __allowances[_from][msg.sender] >= _value && 
+            __balanceOf[_from] >= _value) {
+            __balanceOf[_from] -= _value;
+            __balanceOf[_to] += _value;
+            // Missed from the video
+            __allowances[_from][msg.sender] -= _value;
+            return true;
+        }
+        return false;
+    }
+    
+    function approve(address _spender, uint _value) public returns (bool success) {
+        __allowances[msg.sender][_spender] = _value;
+        return true;
+    }
+    
+    function allowance(address _owner, address _spender) public constant returns (uint remaining) {
+        return __allowances[_owner][_spender];
     }
 }
